f7e3c6287a3c102218c180b55a6ebc7f1d24b3b7
gnodiah <haidongrun@gmail.com>
2014-05-05 17:31:24 +0800

    restore issue:ActiveRecord::ConnectionTimeoutError at / could not obtain a database connection within 5.000 seconds (waited 5.000 seconds)

--- gitmate.rb
+++ gitmate.rb
@@ -3,6 +3,8 @@ require 'sqlite3'
 require 'slim'
 require_relative 'lib/author'
 
+after { ActiveRecord::Base.connection.close }
+
 get '/' do
 	# TODO: 初始化数据库应该只执行一次！想想应该放在什么地方
 	# Author.create_authors
